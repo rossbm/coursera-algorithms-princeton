18482f3c2388aa652b6ec3f47d615dec1f5d4607
Ross Beck-MacNeil <wertu234@gmail.com>
2018-11-12 19:41:35 -0500

    Completed week4, 8puzzle

--- 8puzzle/Board.java
+++ 8puzzle/Board.java
@@ -1,72 +1,271 @@
+import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.LinkedBag;
 
 public final class Board {
     // construct a board from an n-by-n array of blocks
     // (where blocks[i][j] = block in row i, column j)
     private int[] blocks;
-    private int pub_dim;
-    private int dim;
+    // Will have a flattened representation of the square
+    private int squareDim;
+    private int flatDim;
+    // For caching
+    private int manhattanPriority;
+    private int hammingPriority;
+    private boolean isSolved = true;
+    private boolean beenChecked;
+    private int zeroPosition;
     public Board(int[][] blocks) {
-        this.pub_dim = blocks[0].length;
-        this.dim = (int) Math.pow(this.pub_dim, 2);
-        this.blocks = new int[this.dim];
-        for (int i = 0; i < this.pub_dim; i++) {
-            for (int j = 0; j < this.pub_dim; j++) {
-                this.blocks[(i * this.pub_dim ) + j] = blocks[i][j];
+        this.squareDim = blocks[0].length;
+        // Going to use a flattened array to represent
+        // Adding 1, since easier to start at 1;
+        // Will leave 0 index blank
+        this.flatDim = (int) Math.pow(this.squareDim, 2) + 1;
+        this.blocks = new int[this.flatDim];
+        for (int i = 0; i < this.squareDim; i++) {
+            for (int j = 0; j < this.squareDim; j++) {
+                this.blocks[(i * this.squareDim) + j + 1] = blocks[i][j];
             }
         }
     }
+
+    // It is a private method to prevent complaints about the API
+    // It incorporates swapping
+    // Not really certain if the grader will stil detect this and complain
+    private Board(int[] orgBlocks, int idxA, int idxB) {
+        this.flatDim = orgBlocks.length;
+        this.squareDim = (int) Math.sqrt(this.flatDim - 1);
+        this.blocks = new int[this.flatDim];
+        // Copy original blocks
+        for (int i = 0; i < this.flatDim; i++) {
+            this.blocks[i] = orgBlocks[i];
+        }
+        int tmpVal = this.blocks[idxA];
+        this.blocks[idxA] = this.blocks[idxB];
+        this.blocks[idxB] = tmpVal;
+    }
+
+    // TODO: Implement a method to go from a 2d board to a 1d representation
+    // Why?
+    // private int[] flattenBoard(int[][] squareBoard){
+    //
+    // }
+
     // board dimension n
     public int dimension() {
-        return this.dim;
+        return this.squareDim;
     }
-    // number of blocks out of place
+
     public int hamming() {
-        //TODO: Actually calculate
-        return 99;
+        // number of blocks out of place
+        if (!beenChecked) {
+            checkState();
+        }
+        return this.hammingPriority;
     }
     // sum of Manhattan distances between blocks and goal
     public int manhattan() {
-        //TODO: Actually calculate
-        return 99;
+        if (!beenChecked) {
+            checkState();
+        }
+        return this.manhattanPriority;
     }
+
+    // return the column based on flat index
+    private int getColumn(int idx) {
+        return ((idx -1) % this.squareDim) + 1;
+    }
+
+    private int getRow(int idx) {
+        return ((idx -1) / this.squareDim) + 1;
+    }
+
     // is this board the goal board?
     public boolean isGoal() {
-        int prev_piece;
-        int curr_piece;
-        for (int i = 0; i < this.dim; i++) {
-            //doesn't matter if the previous piece is a 0
-            if (prev_piece != 0) {
-                if (curr_piece == 0) {
-                    prev_piece = curr_piece;
+        if (!beenChecked) {
+            checkState();
+        }
+        return this.isSolved;
+    }
+
+    // Will need to check position of zero block
+    private int getZeroPosition() {
+        if (!beenChecked) {
+            checkState();
+        }
+        return this.zeroPosition;
+    }
+
+    // private method to ascertain if stuff has happened to avoid recalculating
+    private void checkState() {
+        int manhattanDist;
+        this.beenChecked = true;
+        for (int i = 1; i < this.flatDim; i++) {
+            int currPiece = this.blocks[i];
+            if (currPiece != i) {
+                if (currPiece != 0) {
+                    // Increment hamming priority
+                    this.hammingPriority += 1;
+                    // Manahattan distance is equal to sum of absolute values of
+                    // difference between row and column indexes
+                    manhattanDist = Math.abs(getColumn(currPiece) - getColumn(i))
+                            + Math.abs(getRow(currPiece) - getRow(i));
+                    this.manhattanPriority += manhattanDist;
                 }
-                else if (this.blocks[i] != (prev_piece + 1)) {
-                    return false;
+                else {
+                    this.zeroPosition = i;
                 }
             }
         }
-        return true;
+        if (this.hammingPriority > 0) {
+            this.isSolved = false;
+        }
     }
+
+    // This method goes returns the flattend index based on column and row
+    private int getIndex(int row, int col) {
+        return ((row -1) * this.squareDim) + col;
+    }
+
     // a board that is obtained by exchanging any pair of blocks
     public Board twin() {
-
+        if (!beenChecked) {
+            checkState();
+        }
+        // default to swapping the square in (1,1) with the square (1,2);
+        // position of the 0 square will allow us to deviate from default;
+        if (this.zeroPosition != 1) {
+            if (this.zeroPosition != 2) {
+                // don't need to use getIndex, since no matter squareDim
+                // this will be the flattened dims
+                return new Board(this.blocks, 1, 2);
+            }
+            else {
+                // swao (1,1) with (2,1)
+                return new Board(this.blocks, 1, getIndex(2,1));
+            }
+        }
+        else {
+            // swap (1,2) with (2,2)
+            return new Board(this.blocks, 2, getIndex(2,2));
+        }
     }
+
+    // Takes a row and column index and returns and integer iterable of the
+    // Flattened indexes of the neighbours of the input
+    // TODO: Make private, remove from main, potentialy rename
+    private Iterable<Integer> getNeighbors(int row, int col) {
+        LinkedBag<Integer> neighbors = new LinkedBag<Integer>();
+        if (row < this.squareDim) {
+            neighbors.add(getIndex(row + 1, col));
+        }
+        if (row > 1) {
+            neighbors.add(getIndex(row - 1, col));
+        }
+
+        if (col < this.squareDim) {
+            neighbors.add(getIndex(row, col+1));
+        }
+        if (col > 1) {
+            neighbors.add(getIndex(row, col-1));
+        }
+        return neighbors;
+    }
+
+    // TODO: Implement, will need to check online on deets
     // does this board equal y?
-    public boolean equals(Object y) {
-
+    public boolean equals(Object other) {
+        if (other == this) return true;
+        if (other == null) return false;
+        if (other.getClass() != this.getClass()) return false;
+        Board that = (Board) other;
+        int n = this.flatDim;
+        if (n != that.flatDim) return false;
+        for (int i = 1; i < n; i++) {
+            if (this.blocks[i] != that.blocks[i]) return false;
+        }
+        return true;
     }
+
     // all neighboring boards
     public Iterable<Board> neighbors() {
-
+        LinkedBag<Board> neighbors = new LinkedBag<Board>();
+        // Want neighbors of 0 block, so get its index
+        int zeroIndex = this.getZeroPosition();
+        Iterable<Integer> neighborIndex = getNeighbors(getRow(zeroIndex), getColumn(zeroIndex));
+        for (int index : neighborIndex) {
+            neighbors.add(new Board(this.blocks, zeroIndex, index));
+        }
+        return neighbors;
     }
     // string representation of this board (in the output format specified below)
     public String toString() {
-        StdOut.println(this.dim);
-        for (int i = 0; i < this.dim)
-
+        StringBuilder sb = new StringBuilder();
+        sb.append(this.squareDim);
+        sb.append("\n");
+        for (int i = 1; i < this.flatDim; i++) {
+            sb.append(String.format("%2d", this.blocks[i]));
+            sb.append(" ");
+            if (i % this.squareDim == 0) {
+                sb.append("\n");
+            }
+        }
+        return sb.toString();
     }
     // unit tests (not graded)
     public static void main(String[] args) {
+        String filename = "puzzle3x3-15.txt";
+        In in = new In(filename);
+        int n = in.readInt();
+        int[][] tiles = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                tiles[i][j] = in.readInt();
+            }
+        }
+        Board initial = new Board(tiles);
+        StdOut.println(initial.toString());
+        StdOut.println("Hamming distance of " + initial.hamming());
+        StdOut.println("Manahattan distance of " + initial.manhattan());
+        StdOut.println("Is goal: " + initial.isGoal() + "\n");
+
+        StdOut.println("The twin of this board is:");
+        Board twin = initial.twin();
+        StdOut.println(twin.toString());
+
+        StdOut.println("The twin of this board equals the original:");
+        StdOut.println(initial.equals(twin));
+
+        StdOut.println("The twin's twin:");
+        Board twin2 = twin.twin();
+        StdOut.println(twin2.toString());
+
+        StdOut.println("The twin's twin equals the original:");
+        StdOut.println(initial.equals(twin2));
+
+        // StdOut.println("The index neigbours of the top right corner are:");
+        // Iterable<Integer> neighborsFirst = initial.getNeighbors(1, 1);
+        // for (int index : neighborsFirst) {
+        //     StdOut.println(index);
+        // }
+        //
+        // StdOut.println("The index neigbours of the bottom right corner are:");
+        // Iterable<Integer> neighborsLast = initial.getNeighbors(n, n);
+        // for (int index : neighborsLast) {
+        //     StdOut.println(index);
+        // }
+        //
+        // StdOut.println("The index neigbours of the middle are:");
+        // Iterable<Integer> neighborsMiddle = initial.getNeighbors(n/2 + 1, n/2 +1);
+        // for (int index : neighborsMiddle) {
+        //     StdOut.println(index);
+        // }
+
+        StdOut.println("The neigbours of the initial board are:");
+        Iterable<Board> neighbors = initial.neighbors();
+        for (Board board : neighbors) {
+            StdOut.println(board.toString());
+        }
 
     }
 }
--- 8puzzle/Solver.java
+++ 8puzzle/Solver.java
@@ -1,46 +1,171 @@
-/* *****************************************************************************
- *  Name:
- *  Date:
- *  Description:
- **************************************************************************** */
-
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.LinkedBag;
+import edu.princeton.cs.algs4.MinPQ;
+import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.StdOut;
 
 public class Solver {
+
+    // Will this to false if twin can be solved
+    // Will also return
+    private boolean solveable = true;
+    private int moves;
+    private Stack<Board> path;
     // find a solution to the initial board (using the A* algorithm)
     public Solver(Board initial) {
 
-    }
-    // is the initial board solvable?
-    public boolean isSolvable() {
+        if (initial == null) throw new java.lang.IllegalArgumentException();
 
+        // This is main searcher
+        SearchTree solutionFinder = new SearchTree(initial);
+        // This is for verifying that a solution does exist
+        SearchTree twinFinder = new SearchTree(initial.twin());
+
+        while (this.solveable) {
+            // Alternate between tree that will find actual solution
+            if (solutionFinder.step()) {
+                this.path = solutionFinder.getPath();
+                // Don't want to count initial board as a "move"
+                this.moves = this.path.size() -1;
+                break;
+            }
+            // And tree that checks if twin has a solution;
+            else {
+                this.solveable = !twinFinder.step();
+                this.moves = -1;
+                this.path = null;
+            }
+        }
+
+    }
+    // //is the initial board solvable?
+    public boolean isSolvable() {
+        return this.solveable;
     }
     // min number of moves to solve initial board; -1 if unsolvable
     public int moves() {
-
+        return this.moves;
     }
     // sequence of boards in a shortest solution; null if unsolvable
     public Iterable<Board> solution() {
-
+        return path;
     }
 
-    public static void main(String[] args) {
-        // create initial board from file
-        In in = new In(args[0]);
-        int n = in.readInt();
-        int[][] blocks = new int[n][n];
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < n; j++)
-                blocks[i][j] = in.readInt();
-        Board initial = new Board(blocks);
-        // solve the puzzle
-        Solver solver = new Solver(initial);
-        // print solution to standard output
-        if (!solver.isSolvable())
-            StdOut.println("No solution possible");
-        else {
-            StdOut.println("Minimum number of moves = " + solver.moves());
-            for (Board board : solver.solution())
-                StdOut.println(board);
+    // Wrapper class for tree
+    // Implemented since alternating back and forth between two trees
+    private class SearchTree {
+        private MinPQ<SearchNode> searchTree = new MinPQ<SearchNode>();
+        private SearchNode latestNode;
+        private int moves;
+
+        public SearchTree(Board initial) {
+            // Initialize search node with 0 moves and no previous board
+            // Consider initializing seatch tree with at least 4 points
+            searchTree.insert(new SearchNode(initial, 0, null));
+        }
+
+        public boolean step() {
+            this.moves++;
+            // The dequed board (SearchNode) with the lowest priority
+            this.latestNode = searchTree.delMin();
+            if (latestNode.isGoal()) {
+                return true;
+            }
+            else {
+                for (SearchNode neighborNode : latestNode.neighbors()) {
+                    this.searchTree.insert(neighborNode);
+                }
+                return false;
+            }
+        }
+
+        public Stack<Board> getPath() {
+            // chase pointers
+            Stack<Board> path = new Stack<Board>();
+            SearchNode node = this.latestNode;
+            while (node != null) {
+                path.push(node.board);
+                node = node.parentNode;
+            }
+            return path;
         }
     }
+
+    // Class that wraps board?
+    // For adding to heap?
+    private class SearchNode implements Comparable<SearchNode> {
+        // This class is mainly used to keep track of priority
+        // The compare to method will make use of priority;
+        private int priority;
+        // priority is based on moves
+        private int moves;
+        // Need to keep track of previous board to avoid adding it to search tree
+        private SearchNode parentNode;
+        // Should probably not be public, should wrape all necessary methods
+        private Board board;
+        public SearchNode(Board board, int moves, SearchNode parentNode) {
+            this.board = board;
+            this.parentNode = parentNode;
+            this.moves = moves;
+            // TODO: Implement way of choosing priority function
+            this.priority = board.manhattan() + this.moves;
+        }
+        // TODO: Better way of breaking ties
+        public int compareTo(SearchNode that) {
+            return Integer.compare(this.priority, that.priority);
+        }
+
+        public Board getBoard() {
+            return this.board;
+        }
+
+        public boolean isGoal() {
+            return this.board.isGoal();
+        }
+
+        public Iterable<SearchNode> neighbors() {
+            Iterable<Board> closeBoards = this.board.neighbors();
+            LinkedBag<SearchNode> closeNodes = new LinkedBag<>();
+            SearchNode node;
+            Board parentBoard;
+            // deal with starting null parentNode
+            if (this.parentNode == null) {
+                parentBoard = null;
+            } else {
+                parentBoard = parentNode.getBoard();
+            }
+
+            for (Board closeBoard : closeBoards) {
+                if (!closeBoard.equals(parentBoard)) {
+                    node = new SearchNode(closeBoard, this.moves + 1, this);
+                    closeNodes.add(node);
+                }
+            }
+            return closeNodes;
+        }
+    }
+
+public static void main(String[] args) {
+    // create initial board from file
+    String filename = "puzzle36.txt";
+    // In in = new In(args[0]);
+    In in = new In(filename);
+    int n = in.readInt();
+    int[][] blocks = new int[n][n];
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            blocks[i][j] = in.readInt();
+    Board initial = new Board(blocks);
+    // solve the puzzle
+    Solver solver = new Solver(initial);
+
+    // print solution to standard output
+    if (!solver.isSolvable())
+        StdOut.println("No solution possible");
+    else {
+        StdOut.println("Minimum number of moves = " + solver.moves());
+        for (Board board : solver.solution())
+            StdOut.println(board);
+    }
+}
 }
